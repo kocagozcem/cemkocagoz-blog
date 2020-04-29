f84c53cd6cdbec7910975df1fbb451eb7560b84f
kocagozcem <kocagozcem@gmail.com>
2020-04-30 00:28:41 +0300

    blog page created

---
+++ pages/blog.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout/layout';
+
+// var classNames = require('classnames');
+// https://github.com/JedWatson/classnames
+
+export default function Blog(){
+    return(
+        <div className="container">
+            <Head>
+                <title>Cem Kocagöz | Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Layout>
+                <h1>Blog page</h1>
+                <h3>
+                    <Link href="/">
+                        <a>Back to home</a>
+                    </Link>
+                </h3>
+            </Layout>
+        </div>
+        
+    )
+}
