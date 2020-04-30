a105160b274fabd61e3cb63fab8b247022136cd3
kocagozcem <kocagozcem@gmail.com>
2020-04-30 03:35:27 +0300

    menu version 0.1

--- components/menu/menu.js
+++ components/menu/menu.js
@@ -1,16 +1,28 @@
 import styles from './menu.module.scss'
+import Link from 'next/link'
+
+var classNames = require('classnames');
 
 function NavigationMenu({ children }) {
   return (
     <div className={styles.container}>
       <div className={styles.logoContainer}>
-        <h2 className={styles.logo}>Cem Kocagöz</h2>
+        <div className={styles.logo}>
+          <h2 className={styles.logoName}>Cem</h2>
+          <h2 className={styles.logoSurname}> Kocagöz</h2>
+        </div>
       </div>
       <nav>
         <ul className={styles.ul}>
-          <li className={styles.menuItem}>Anasayfa</li>
-          <li className={styles.menuItem}>TIL Blog</li>
-          <li className={styles.menuItem}>Hakkımda</li>
+          <Link href="/">
+            <li className={styles.menuItem}>Anasayfa</li>
+          </Link>
+          <Link href="blog">
+            <li className={styles.menuItem}>TIL Blog</li>
+          </Link>
+          <Link href="/">
+            <li className={styles.menuItem}>Hakkımda</li>
+          </Link>
         </ul>
       </nav>
     </div>
