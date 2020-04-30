c1a64eb8c9dee0e1e9b1bbd617ccdba784c344b6
kocagozcem <kocagozcem@gmail.com>
2020-05-01 01:05:48 +0300

    google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout/layout'
+import AboutMe from '../components/aboutme/aboutme';
+import { useEffect } from 'react';
 
 export default function Home() {
+
+
   return (
     <div className="container">
       <Head>
         <title>Cem Kocagöz | Anasayfa</title>
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-146867983-1');`,
+          }}
+        />
       </Head>
 
       <main>
         <Layout>
-
+          <AboutMe />
         </Layout>
       </main>
 
