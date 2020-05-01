2aeb326de3eeee3cdaad555b5fb236983c4e1d82
kocagozcem <kocagozcem@gmail.com>
2020-05-01 23:01:18 +0300

    description meta tag for seo

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,11 @@ export default function Home() {
             gtag('config', 'UA-146867983-1');`,
           }}
         />
+
+        <meta 
+          name="description"
+          content="I'm Cem Kocagoz and I'm a front-end developer who focuses on improving himself about developing mobile and web applications. Since the last quarter of 2018, I have been working and learning with Angular and React Native technologies. I'm graduated from Ege University Computer Programming associate's degree and took courses about C,  Python, PHP, Pascal, Delphi, SQL and Visual Basic technologies but didn't go deeper about them."
+          />
       </Head>
 
       <main>
