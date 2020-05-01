71370250755436cd5a7da1d53b85ae2f9a607552
kocagozcem <kocagozcem@gmail.com>
2020-05-01 23:26:13 +0300

    get about me from json file

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../components/layout/layout'
 import AboutMe from '../components/aboutme/aboutme';
-import { useEffect } from 'react';
 import CurrentPosition from '../components/currentposition/current-position';
 import Github from '../components/github/github';
+import { aboutme } from '../assets/aboutme.json';
 
 export default function Home() {
 
 
   return (
-    <div className="container">
+    <html lang="en">
       <Head>
         <title>Cem Kocagöz | Anasayfa</title>
         <link rel="icon" href="/favicon.ico" />
@@ -23,28 +23,29 @@ export default function Home() {
             gtag('config', 'UA-146867983-1');`,
           }}
         />
-
         <meta 
           name="description"
-          content="I'm Cem Kocagoz and I'm a front-end developer who focuses on improving himself about developing mobile and web applications. Since the last quarter of 2018, I have been working and learning with Angular and React Native technologies. I'm graduated from Ege University Computer Programming associate's degree and took courses about C,  Python, PHP, Pascal, Delphi, SQL and Visual Basic technologies but didn't go deeper about them."
+          content={aboutme}
           />
       </Head>
+      <div className="container">
+        <main>
+          <Layout>
+            <AboutMe/>
+            <CurrentPosition/>
+            <Github/>
+          </Layout>
+        </main>
 
-      <main>
-        <Layout>
-          <AboutMe/>
-          <CurrentPosition/>
-          <Github/>
-        </Layout>
-      </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          display: flex;
-          flex-direction: column;
-        }
-      `}</style>
-    </div>
+        <style jsx>{`
+          .container {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+          }
+        `}</style>
+      </div>
+    </html>
+    
   )
 }
